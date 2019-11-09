e2ffb1d5842741b0baa253d373c60ff35bbd39d8
dcgarde <garde.danielle@gmail.com>
2019-11-08 21:47:54 -0600

    edited files

--- src/absclass/Main.java
+++ src/absclass/Main.java
@@ -2,13 +2,13 @@ package absclass;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(4,3);
-        Circle circle = new Circle(3);
-        Square square = new Square(5);
+        Shape s1 = new Rectangle(4,3);
+        Shape s2 = new Circle(3);
+        Shape s3 = new Square(5);
 
-        rectangle.area();
-        circle.area();
-        square.area();
+        s1.area();
+        s2.area();
+        s3.area();
     }
 
 }
