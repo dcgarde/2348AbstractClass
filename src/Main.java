4af9ae1a7c4adae9c8eaf8eebea834c293e70bcd
dcgarde <garde.danielle@gmail.com>
2019-11-08 21:12:04 -0600

    edited main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        Circle circle = new Circle();
+        Square square = new Square();
+
+        rectangle.area();
+        circle.area();
+        square.area();
     }
 
 }
