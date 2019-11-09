d933e6548ab27704ff2e2cac45aaa6a96092e8f0
dcgarde <garde.danielle@gmail.com>
2019-11-08 21:36:45 -0600

    split classes into separate files

--- src/absclass/Main.java
+++ src/absclass/Main.java
@@ -1,32 +1,10 @@
 package absclass;
 
-abstract class Shape {
-    abstract void area();
-}
-
-class Rectangle extends Shape {
-    public void area(){
-        System.out.println("The area of the rectangle is 10");
-    }
-}
-
-class Circle extends Shape {
-    public void area(){
-        System.out.println("The area of the circle is 4.32");
-    }
-}
-
-class Square extends Shape {
-    public void area() {
-        System.out.println("The area of the square is 15.");
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle();
-        Circle circle = new Circle();
-        Square square = new Square();
+        Rectangle rectangle = new Rectangle(4,3);
+        Circle circle = new Circle(3);
+        Square square = new Square(5);
 
         rectangle.area();
         circle.area();
